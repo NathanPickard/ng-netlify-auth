b6a3f7ba84c62e6c0639a3043b465bc4b7d959aa
Nathan Pickard <nathan.pickard87@gmail.com>
2020-12-02 21:38:34 -0800

    Added scully package

---
+++ scully.ng-netlify-auth.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "ng-netlify-auth",
+  outDir: './dist/static',
+  routes: {
+  }
+};
