593d5b1f3896bc2096b2a2bba004f410fb139c1e
Nathan Pickard <nathan.pickard87@gmail.com>
2020-12-03 21:06:38 -0800

    Typo correction in index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+
 import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
